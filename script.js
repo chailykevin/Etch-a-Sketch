40f79138b08e75dae695e4dd56670bb0466105ad
chailykevin <chailykevin@gmail.com>
2024-02-01 23:05:11 +0700

    Update index.html, style.css, script.js
    - Add grid button to update grid
    - Add a trail effect event listener to grid
    - Add deleteGrid and updateGrid function
    - Minor changes to index.html and style.css

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const container = document.querySelector("#container");
 
 //Add 16x16 grid if page is loaded
-window.addEventListener("DOMContentLoaded", () => {
-    for(let i = 0 ; i < 16 ; i++){
-        for(let j = 0 ; j <16;j++){
+window.addEventListener("DOMContentLoaded", updateGrid(16));
+
+//Add a "trail" like effect, whenever a div is hovered by mouse, the background color will change
+container.addEventListener("mouseenter", () => {
+    const div = container.childNodes;
+
+    div.forEach((currentDiv) => {
+        currentDiv.addEventListener("mouseenter", () => {
+            currentDiv.classList.add("hovered");
+        })
+    })
+})
+
+//Asked the user to input a new grid value
+const addGrid = document.querySelector("#grid");
+
+addGrid.addEventListener("click", () => {
+    deleteGrid();
+    updateGrid(prompt("Input how big the grid is gonna be : "));
+})
+
+//Function to update grid
+function updateGrid(value) {
+    for(let i = 0 ; i < value ; i++){
+        for(let j = 0 ; j < value ; j++){
             container.appendChild(document.createElement("div"));
         }
     }
-});
+    
+    const divs = container.children;
+    for(let i = 0 ; i < divs.length ; i ++) {
+        divs[i].style.minWidth = 100/value + "%";
+        divs[i].style.minHeight = 100/value + "%";
+    }
+}
+
+//Function to delete grid
+function deleteGrid() {
+    container.innerHTML = "";
+}
